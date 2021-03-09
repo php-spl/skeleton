40562d166ca32d1bc19d72b37c87dd0b66662b71
Thomas Elvin <info@datalaere.dk>
2021-03-09 15:29:23 +0100

    update
    update

--- app/functions.php
+++ app/functions.php
@@ -1,9 +1,13 @@
 <?php
 
 function e($text) {
-    echo htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
+   echo htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
  }
  
- function redirect($url){
-    return header('Location: ' . $url);
+ function redirect($url) {
+   return header('Location: ' . $url);
+ }
+
+ function dump() {
+   return var_dump(func_get_args());
  }
