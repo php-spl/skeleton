46456e645f6ad923d86d2a8a7f749caac6cb4cfd
Thomas Elvin <info@datalaere.dk>
2021-03-22 19:22:35 +0100

    Update services.php

--- config/services.php
+++ config/services.php
@@ -1,36 +1,47 @@
 <?php
 
+use Web\DI\DIContainer;
+
 // Load container
-$container = new Web\DI\Container;
+$container = new DIContainer;
 
 // Load config
-$container->set('config', new Web\Config\Config());
-$container->Config->load(ABSPATH . '/config/app.php');
+$container->set('config', function() {
+    $config = new Web\Config\Config;
+    $config->load(ABSPATH . '/config/app.php');
+    return $config;
+});
 
 // Load sesion
-$container->set('session', new Web\Session\Session);
+$container->set('session', function() {
+    return new Web\Session\NativeSession;
+});
 
 // Load CSRF
-$container->set('csrf', new Web\Security\CSRF($container->Config->get('app.key')));
+$container->set('csrf', function(DIContainer $c) {
+   return new Web\Security\CSRF($c->config->get('app.key'));
+});
 
 // Load view
-$container->set('view', new Web\MVC\View($container));
-$container->View->setViewsPath(ABSPATH . '/app/views/');
+$container->set('view', function(DIContainer $c) {
+    $view = new Web\MVC\View($c);
+    return $view->setViewsPath(ABSPATH . '/app/views/');
+});
 
 // Load error handler
-$container->set('errors', new Web\Log\Errors);
+$container->set('errors', function() {
+    return new Web\Log\Errors;
+});
 
-// Load database
-$container->set('db', new Web\Database\Database(
-    $container->Config->db
-    )
-);
+// Load error handler
+$container->set('db', function(DIContainer $c) {
+    return new Web\Database\Database($c->config->db);
+});
 
 // Load validator
-$container->set('validator', new Web\Security\Validator(
-    $container->DB,
-    $container->Errors
-));
+$container->set('validator', function(DIContainer $c) {
+    return new Web\Security\Validator($c->db, $c->errors);
+});
 
 
 return $container;
