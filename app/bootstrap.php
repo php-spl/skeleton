354d87fe8f64c971fb298edb5af74f2f4bd48e4d
Thomas Elvin <info@datalaere.dk>
2021-04-27 22:42:30 +0200

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -32,7 +32,7 @@ require_once ROOT_PATH . '/vendor/autoload.php';
 | Create The Application
 |--------------------------------------------------------------------------
 |
-| The first thing we will do is create a new Laravel application instance
+| The first thing we will do is create a new application instance
 | which serves as the "glue" for all the components of the web app, and is
 | the IoC container for the system binding all of the various parts.
 |
@@ -53,10 +53,10 @@ require_once ROOT_PATH . '/app/helpers.php';
 
 /*
 |--------------------------------------------------------------------------
-| Setup environment
+| Setup environment and load Config
 |--------------------------------------------------------------------------
 |
-| Check if application is in development or production mode.
+| Load environment and load Config files
 |
 */
 
